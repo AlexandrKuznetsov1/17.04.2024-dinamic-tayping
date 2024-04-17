029b14a919b51c7b8d1e245c467d0208ab6672cf
AlexandrKuznetsov1 <166503929+AlexandrKuznetsov1@users.noreply.github.com>
2024-04-17 12:09:39 +0700

    Add files via upload

---
+++ homework2.2.py
@@ -0,0 +1,13 @@
+name = 'Name: Aleksandr'
+print(name)
+age = 48
+age = ("Age:"+str(age))
+print( age)
+age = 48
+new_age = (age)+1
+new_age = ("New age:"+str(new_age))
+print(new_age)
+is_student = True
+is_student =('Is student:'+str(is_student))
+print(is_student)
+print('P.S. И день рождения отметим вместе;-)')
